fd959f4ca00c1fe9c7caf3da68bb089f2f20c5da
Elizabeth Cannon <elizabeth230@gmail.com>
2019-01-28 19:51:49 -0800

    Place Xs, Os still not working

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -5,7 +5,28 @@ var move = function(elementId) {
 	document.getElementById(`${elementId}`).style.color = "red";
 };
 
+var gamePieces = 0;
+
+var placePiece = function(gamePieces) {
+	console.log(gamePieces);
+	var piece;
+	if (gamePieces % 2 === 0) {
+		piece = 'X';
+	} else {
+		piece = 'O';
+	}
+	gamePieces += 1;
+	return piece;
+}
+
 document.addEventListener("click", function(e) {
 	if (e.target.className === "tableCell")
-	alert(e.target.id);
+	console.log(e.target);
+	var id = e.target.id;
+	console.log('text content: ', document.getElementById(id).textContent);
+	var textContent = document.getElementById(id).textContent;
+	if (!textContent) {
+		var text = document.createTextNode(placePiece(gamePieces));
+		document.getElementById(id).appendChild(text);
+	}
 });

e8609d62a48fd8bb6787294a26af2f5c334a2628
Elizabeth Cannon <elizabeth230@gmail.com>
2019-01-28 17:29:21 -0800

    Generate Table within html

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,29 @@
+// console.log('work?');
 
+// var h1 = document.createElement('h1');
+// var title = document.createTextNode('Tic Tac Toe');
+// h1.appendChild(title);
+// document.body.appendChild(h1);
 
+// var div = document.createElement('div');
+// var text = document.createTextNode('Board');
+// div.appendChild(text);
+// document.body.appendChild(div);
+
+// var table = document.createElement('table');
+
+// var tbody = document.createElement('tbody');
+// for (var i = 0; i < 3; i++) {
+// 	var row = document.createElement('tr');
+// 	for (var j = 0; j < 3; j++) {
+// 		var cell = document.createElement('td');
+// 		var cellText = document.createTextNode(i + ',' + j);
+// 		cell.appendChild(cellText);
+// 		row.appendChild(cell);
+// 	}
+// 	tbody.appendChild(row);
+// }
+
+// table.appendChild(tbody);
+
+// document.body.appendChild(table);

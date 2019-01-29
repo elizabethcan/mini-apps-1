023b6a198b20542690b29b34cdb32b3ab16d1832
Elizabeth Cannon <elizabeth230@gmail.com>
2019-01-28 19:30:19 -0800

    Add eventListener to all table cells

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,29 +1,11 @@
-// console.log('work?');
+console.log('work?');
 
-// var h1 = document.createElement('h1');
-// var title = document.createTextNode('Tic Tac Toe');
-// h1.appendChild(title);
-// document.body.appendChild(h1);
+var move = function(elementId) {
+	console.log(`${elementId}`);
+	document.getElementById(`${elementId}`).style.color = "red";
+};
 
-// var div = document.createElement('div');
-// var text = document.createTextNode('Board');
-// div.appendChild(text);
-// document.body.appendChild(div);
-
-// var table = document.createElement('table');
-
-// var tbody = document.createElement('tbody');
-// for (var i = 0; i < 3; i++) {
-// 	var row = document.createElement('tr');
-// 	for (var j = 0; j < 3; j++) {
-// 		var cell = document.createElement('td');
-// 		var cellText = document.createTextNode(i + ',' + j);
-// 		cell.appendChild(cellText);
-// 		row.appendChild(cell);
-// 	}
-// 	tbody.appendChild(row);
-// }
-
-// table.appendChild(tbody);
-
-// document.body.appendChild(table);
+document.addEventListener("click", function(e) {
+	if (e.target.className === "tableCell")
+	alert(e.target.id);
+});

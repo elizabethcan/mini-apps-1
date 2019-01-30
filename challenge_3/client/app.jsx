951801e430929197967af36fc5ce463afc5b7a26
Elizabeth Cannon <elizabeth230@gmail.com>
2019-01-30 14:44:46 -0800

    Add components to app.jsx to render the first checkout button and form

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,4 +1,47 @@
 // you MUST place all of your React components into one file, app.jsx
 
+class UserForm extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById('app'));
+  render() {
+    return (
+      <div>
+        <div>
+          <div>Name:</div>
+          <input className="userFormInput" id="userName"></input>
+        </div>
+        <div>
+          <div>Username:</div>
+          <input className="userFormInput" id="userEmail"></input>
+        </div>
+        <div>
+          <div>Password:</div>
+          <input className="userFormInput" id="userPassword"></input>
+        </div>
+      </div>
+    )
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>Shopping Cart</h1>
+        </div>
+        <button className="checkOutBtn">Check Out</button>
+        <UserForm />
+        <button>Next</button>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));

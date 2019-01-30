f45258280cdd96469e048f7d865475e2b1c6b7db
Elizabeth Cannon <elizabeth230@gmail.com>
2019-01-30 11:55:26 -0800

    Require express in server.js

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,7 @@
 // Build your Express app inside server.js
+// Use Express to serve up an index.html file and its associated assets
 
+var express = require('express');
+var app = express();
+
+app.use(epress.static('public'));

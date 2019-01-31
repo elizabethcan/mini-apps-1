731e4bc2e22562f05e89e5c4ee17842542b0d227
Elizabeth Cannon <elizabeth230@gmail.com>
2019-01-31 15:21:32 -0800

    Create express server

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 
+app.get('/', (req, res) => {
+  res.send('hello from server');
+});
+
+app.listen(port, () => console.log(`now listening on port: ${port}`));
 

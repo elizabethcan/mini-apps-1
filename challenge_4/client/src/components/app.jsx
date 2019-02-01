977e18cea9721f7d21d2d96300e3c59e42969302
Elizabeth Cannon <elizabeth230@gmail.com>
2019-01-31 16:10:30 -0800

    Add TableColumn component and css to render board

--- challenge_4/client/src/components/app.jsx
+++ challenge_4/client/src/components/app.jsx
@@ -1,9 +1,34 @@
-function App() {
-  return (
-    <div>
-      <h1>Hello World From Client</h1>
-    </div>
-  );
+import TableColumn from "./tableColumn.jsx";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Connect 4!</h1>
+        <table className="inline-block">
+          <TableColumn />
+        </table>
+        <table className="inline-block">
+          <TableColumn />
+        </table>
+        <table className="inline-block">
+          <TableColumn />
+        </table>
+        <table className="inline-block">
+          <TableColumn />
+        </table>
+        <table className="inline-block">
+          <TableColumn />
+        </table>
+        <table className="inline-block">
+          <TableColumn />
+        </table>
+        <table className="inline-block">
+          <TableColumn />
+        </table>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ challenge_4/client/src/components/tableColumn.jsx
@@ -0,0 +1,28 @@
+class TableColumn extends React.Component {
+  render () {
+    return (
+      <tbody>
+        <tr className="row">
+          <td className="cell">5</td>
+        </tr>
+        <tr className="row">
+          <td className="cell">4</td>
+        </tr>
+        <tr className="row">
+          <td className="cell">3</td>
+        </tr>
+        <tr className="row">
+          <td className="cell">2</td>
+        </tr>
+        <tr className="row">
+          <td className="cell">1</td>
+        </tr>
+        <tr className="row">
+          <td className="cell">0</td>
+        </tr>
+      </tbody>
+    )
+  }
+}
+
+export default TableColumn;
